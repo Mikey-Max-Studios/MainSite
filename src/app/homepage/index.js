e5dcd67559c61a711cbf0da921a338ddd112c40e
Michael Paterson <mike@beamery.com>
2018-05-13 19:20:00 +0100

    rotation

--- src/app/homepage/index.js
+++ src/app/homepage/index.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react';
 // import Counter from './counter';
 // import RequestMaker from './requestMaker';
+import styles from './styles.module.scss';
 
 class Homepage extends PureComponent {
     render () {
         return (
-            <div>
+            <div className={`${styles['title-text']} ${styles.rotating}`}>
                 MCC
             </div>
         );
